9c27816b6fa07408c2beb53a5dd7a7c5971758cc
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2021-09-22 16:09:14 +0900

    Add a fake implementation of setjmp/longjmp to compile forcibly

---
+++ include/setjmp.h
@@ -0,0 +1,5 @@
+typedef void* jmp_buf;
+
+int setjmp(jmp_buf env);
+
+void longjmp(jmp_buf env, int val);
---
+++ src/setjmp.c
@@ -0,0 +1,5 @@
+#include<setjmp.h>
+
+int setjmp(jmp_buf env) { return 0; }
+
+void longjmp(jmp_buf env, int val) {}

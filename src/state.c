53d0836468004ce190f366a6a38204b62d28e94d
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2021-09-22 20:36:14 +0900

    Don't call a MRB_TRY at the end of the script to avoid a Wasm trap

--- src/state.c
+++ src/state.c
@@ -176,13 +176,13 @@ mrb_free_context(mrb_state *mrb, struct mrb_context *c)
   mrb_free(mrb, c);
 }
 
-int mrb_protect_atexit(mrb_state *mrb);
+/* int mrb_protect_atexit(mrb_state *mrb); */
 
   MRB_API void
 mrb_close(mrb_state *mrb)
 {
   if (!mrb) return;
-  mrb_protect_atexit(mrb);
+  /* mrb_protect_atexit(mrb); */
 
   /* free */
   mrb_gc_destroy(mrb, &mrb->gc);
